f2ff3094f9074d742eea52a14469bca131af2230
harits <haritsdeveloper@gmail.com>
2026-01-27 19:32:19 +0700

    Variable di PHP

--- Variable.php
+++ Variable.php
@@ -7,3 +7,16 @@ echo "Nama : ";
 echo "$nama\n";
 echo "Umur : ";
 echo "$age tahun";
+echo "\n";
+
+
+//Variable Variables
+$contoh = "Magnus";
+$$contoh= "keren";
+
+echo "Contoh : ";
+echo $contoh;
+echo "\n";
+
+echo "Magnus ";
+echo $Magnus;
