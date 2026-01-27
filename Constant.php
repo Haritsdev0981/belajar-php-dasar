80afb29b4c0429a9a44bfc1c7440799fb6f3a995
harits <haritsdeveloper@gmail.com>
2026-01-27 19:43:01 +0700

    Constant

---
+++ Constant.php
@@ -0,0 +1,13 @@
+<?php
+
+//Cara agar nilai variable tidak dapat diubah, atau membuat kode php yang mula nya mutable menjadi immutable
+define("AUTHOR", "Haritsdev0981");
+define("APP_VERSION", 100);
+
+echo "Author: ";
+echo AUTHOR;
+echo "\n";
+
+echo "App Version: ";
+echo APP_VERSION;
+echo "\n";
